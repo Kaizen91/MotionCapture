5364ec15c511fd40a51564543c3b35d93aa39cc7
Stephen Lang <stephenlang91@gmail.com>
2020-06-12 16:44:51 -0400

    final touches

---
+++ README.md
@@ -0,0 +1,14 @@
+The purpose of this project is to create a program with python's OpenCv (cv2) module.  The program will capture any movement that passes into the frame of your laptop's webcam and record the time the object entered and exited the frame to a csv file.
+
+requirements:
+python3
+OpenCv module installed
+A Webcam
+
+instructions:
+1.Download the repo and run motion_detection.py
+2.Move out of the frame before the camera starts
+3.Your webcam should turn on.
+4.Move in and out of the frame.
+5.Press "q" to quit.
+6.The times an object entered the frame should be visible in the Times.csv file.
---
+++ motion_detection.py
@@ -0,0 +1,72 @@
+import cv2, time, pandas as pd
+from datetime import datetime
+
+first_frame = None
+status_list=[None,None]
+times=[]
+df = pd.DataFrame(columns=["Start","End"])
+video = cv2.VideoCapture(0)
+
+time.sleep(5) #this is needed for the webcame on my Macbook Pro
+
+while True:
+
+    check, frame = video.read()
+
+    status = 0 #no motion in the current frame
+
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray=cv2.GaussianBlur(gray,(21,21),0)
+
+    #getting a firstframe to use for comparison in finding movement
+    if first_frame is None:
+        first_frame = gray
+        print("FirstFrame:",' ')
+        print(first_frame)
+        continue
+
+    #building the various frames
+    delta_frame=cv2.absdiff(first_frame,gray)
+    thresh_frame=cv2.threshold(delta_frame,30,255,cv2.THRESH_BINARY)[1]
+    thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
+
+    #adding rectangles to moving objects
+    (cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        status=1 #motion found
+        (x, y, w, h)=cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0),3)
+
+    #finds the times that an object entered and exited the frame
+    status_list.append(status) 
+    if status_list[-1]==1 and status_list[-2]==0:
+        times.append(datetime.now())
+    if status_list[-1]==0 and status_list[-2]==1:
+        times.append(datetime.now())
+
+    #creating the windows
+    cv2.imshow("GrayFrame",gray)
+    cv2.imshow("DeltaFrame",delta_frame)
+    cv2.imshow("ThreshFrame",thresh_frame)
+    cv2.imshow("ColourFrame",frame)
+
+    key=cv2.waitKey(1)
+    
+
+    #escapes the look
+    if key==ord("q"):
+        if status==1:
+            times.append(datetime.now())
+        break
+    
+print(times)
+
+#creating a csv file with the times an object entered and left the frame.
+for i in range(0,len(times),2):
+    df = df.append({"Start":times[i],"End":times[i+1]},ignore_index=True)
+
+df.to_csv("Times.csv")
+video.release()
+cv2.destroyAllWindows
